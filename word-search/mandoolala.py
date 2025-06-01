c27a2eaa2b10cb1ddbf1b9c68c2221fb841b2c4c
MJ Kang <mj.kang@paywatchglobal.com>
2025-06-01 18:03:44 +0900

    79. Word Search

---
+++ word-search/mandoolala.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n_rows, n_cols = len(board), len(board[0])
+        traversal = set()
+
+        def dfs(row, col, idx):
+            if idx == len(word):
+                if not (0 <= row < n_rows and 0 <= col < n_cols):
+                    return False
+            if (row, col) in traversal:
+                return False
+            if board[row][col] != word[idx]:
+                return False
+
+            traversal.add((row, col))
+            for (r, c) in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                if dfs(row + r, col + c, idx + 1):
+                    return True
+            traversal.remove((row, col))
+            return False
+
+        for r in range(n_rows):
+            for c in range(n_cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+
